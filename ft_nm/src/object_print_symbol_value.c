6cfd2d46f6e6f6d99bd8130d50c91bd3ce6d4651
Aurelien CAZUC <acazuc@student.42.fr>
2016-09-20 14:36:54 +0200

    Added missing space

--- ft_nm/src/object_print_symbol_value.c
+++ ft_nm/src/object_print_symbol_value.c
@@ -6,7 +6,7 @@
 /*   By: acazuc <acazuc@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/20 12:19:57 by acazuc            #+#    #+#             */
-/*   Updated: 2016/09/20 14:18:54 by acazuc           ###   ########.fr       */
+/*   Updated: 2016/09/20 14:36:27 by acazuc           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,4 +28,5 @@ void	object_print_symbol_value(t_object *object, t_symbol *symbol)
 		else
 			print_hex_4(symbol->value);
 	}
+	ft_putchar(' ');
 }
